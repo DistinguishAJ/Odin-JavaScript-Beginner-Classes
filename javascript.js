3c341c8f0719c001aa4e3c5e32404258c41c1a06
Abraham Josiah <111082843+DistinguishAJ@users.noreply.github.com>
2026-04-11 06:19:26 +0100

    refactor: update calculations and logging in JavaScript file

--- javascript.js
+++ javascript.js
@@ -13,8 +13,35 @@
 
         console.log(age);
 
-        const pi = 3.14;
-        pi = 10;
+        console.log("23 + 97");
 
-        console.log(pi);
-    
+        let sum = 23 + 97;
+        console.log(sum);
+
+        let num = (3 + 2) - 76 * (1 + 1);
+        console.log(num);
+
+        let val = (25 + 50) + 58 - 20;
+        console.log(val);
+
+        let numOne = (30 + 50 + 32 * 8) - 6 * 12 - (23 + 5);
+        console.log(numOne);
+
+        let newVal = (4 + 6 + 9) / 77;
+        console.log(newVal);
+
+        let a = 10;
+        console.log(a);
+
+        a = 20;
+
+        console.log(a);
+
+        let b = 7 * a;
+        console.log(b);
+
+        const max = 57;
+        const actual = max - 13;
+        const percentage = actual / max;
+
+        console.log(percentage);
