5c8e8667ee62e0170633e1bc7bfd8a34184766eb
Abraham Josiah <111082843+DistinguishAJ@users.noreply.github.com>
2026-05-09 06:57:38 +0100

    refactor: clean up JavaScript code and improve readability

--- javascript.js
+++ javascript.js
@@ -1,47 +1,39 @@
-// your JavaScript goes here!
-        console.log("Hello, World!")
-        let firstName = "John";
-        let lastName = "Doe";
+let firstName = "John";
+let lastName = "Doe";
 
-        console.log(firstName);
-        console.log(lastName);
+console.log(firstName);
+console.log(lastName);
 
-        let age = 11;
-        console.log(age);
+let age = 11;
+console.log(age);
 
-        age = 54;
+age = 54;
 
-        console.log(age);
+console.log(age);
 
-        console.log("23 + 97");
 
-        let sum = 23 + 97;
-        console.log(sum);
+let x = 23;
+let y = 97;
 
-        let num = (3 + 2) - 76 * (1 + 1);
-        console.log(num);
+console.log(x + y);
 
-        let val = (25 + 50) + 58 - 20;
-        console.log(val);
+let sum = (4 + 6 + 9) / 77;
+console.log(sum);
 
-        let numOne = (30 + 50 + 32 * 8) - 6 * 12 - (23 + 5);
-        console.log(numOne);
+let a = 10;
+console.log(a);
 
-        let newVal = (4 + 6 + 9) / 77;
-        console.log(newVal);
+a = 58;
+console.log(a);
 
-        let a = 10;
-        console.log(a);
+let b = 7 * a;
+console.log(b);
 
-        a = 20;
+const max = 57;
+console.log(max);
 
-        console.log(a);
+const actual = max - 13;
+console.log(actual);
 
-        let b = 7 * a;
-        console.log(b);
-
-        const max = 57;
-        const actual = max - 13;
-        const percentage = actual / max;
-
-        console.log(percentage);
+const percentage = actual / max;
+console.log(percentage);
